e88f7c75d74a1fe971e428f71795cecb79baa359
s-bose <shiladitya_basu@live.com>
2020-08-07 20:54:06 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'assignments';
+  public defaultCity: string;
+  public userImage: string;
+  public isDisabled: boolean;
+
+  ngOnInit(): void {
+    this.defaultCity = 'Kolkata';
+    this.userImage = './assets/user.png';
+    this.isDisabled = true;
+  }
+
 }
